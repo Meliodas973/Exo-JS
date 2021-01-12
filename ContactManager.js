718b76ff0e56803d46671c3087ef4ff9dab1aba7
Tou Bon SIONG <meliodas45@hotmail.com>
2021-01-12 09:28:13 +0100

    12.01.2021

---
+++ ContactManager.js
@@ -0,0 +1,35 @@
+class ContactManager {
+    constructor(liste) {
+        this.contacts = liste ? liste: [];
+    }
+
+    displayMenu = () => {
+        let choix = prompt("Que souhaitez-vous faire ?\n\n 1 - Lister les contacts\n 2 - Ajouter un nouveau contact\n 3 - Supprimer un contact\n 4 - Quitter le gestionnaire de contacts\n");
+        switch (choix) {
+            case '1':
+                console.log("Entrée 1");
+                this.displayMenu();
+                break;
+            case '2':
+                console.log("Entrée 2");
+                this.displayMenu();
+                break;
+            case '3':
+                console.log("Entrée 3");
+                this.displayMenu();
+                break;
+            case '4':
+                console.log("A bientôt 4");
+                break;
+            default: 
+                console.log('Veillez sectionner un chiffre entre 1 et 4');
+                this.displayMenu();
+                break;
+        }
+    }
+}
+
+//fonction dans contact manager qui va appeller des fonction dans la class contact
+
+
+   
--- main.js
+++ main.js
@@ -2,13 +2,16 @@
 let contact1 = new Contact ('Bourgeois', 'Yoan', 'y.bourgeois@email.com')
 console.log(contact1);
 
-let contact2 = new Contact ('Siong', 'Toubon', 'toubon@email.com')
+let contact2 = new Contact ('Siong2', 'Toubon2', 'toubon@email.com')
 console.log(contact2);
 
-let contact3 = new Contact ('Siong', 'Toubon', 'toubonemail.com')
+let contact3 = new Contact ('Siong3', 'Toubon3', 'toubonemail.com')
 console.log(contact3);
 
-let contact4 = new Contact ('S', 'Toubon', 'toubon@emailcom')
+let contact4 = new Contact ('Siong4', 'Toubon4', 'toubon@emailcom')
 console.log(contact4);
 
 contact1.displayInfo()
+
+let manager = new ContactManager ()
+manager.displayMenu();
